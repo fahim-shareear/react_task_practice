d8d971cb296f3d061d9d8a9bc40cd679c2dedfe0
arctic-shark <fahimshareear@gmail.com>
2025-11-22 03:40:56 +0600

    Updating the App.jsx

--- task-react/src/App.jsx
+++ task-react/src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import Count from './Counts'
-import ShowHide from '../../Show_Hide'
+import ShowHide from './Show_Hide'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
